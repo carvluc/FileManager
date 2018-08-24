61df6856850b48cc95ef70f5770c6c663049bc33
LucasV <vasconcelos.luhh@gmail.com>
2018-08-23 22:45:09 -0300

    Criando projeto base através do CLion para início de desenvolvimento

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <iostream>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+//region Comments
+/*1 bloco - 32 bytes
+1 carac = 1byte, então cada bloco vai ter 32 zeros.
+arquivo de 1MB = 1024/32 = 32blocos*/
+//endregion
+
+//region Types
+
+typedef void(*PointerFunction);
+
+//endregion
+
+//region Classes
+
+class VirtualHD {
+public:
+    string nameHD;
+    int num;
+    double tam;
+
+    void openFile() {
+        try {
+            ofstream fs;
+            fs.open("HardDrive.txt", fstream::out);
+            string formathd = "";
+            fs.write("Lucas", sizeof("Lucas"));
+            fs.close();
+        }
+        catch (exception ex) {
+
+        }
+    }
+
+    VirtualHD() {
+        VirtualHD hd;
+        cin >> hd.nameHD;
+        cin >> hd.tam;
+        cin >> hd.num;
+        openFile();
+    }
+};
+//endregion
+
+//region Static Methods
+
+void teste() {
+    cout << "funciona\n";
+}
+
+
+void inserirAcaoMenu(map<string, PointerFunction>& options) {
+    //options["createhd"] = &createVirtualHD;
+
+}
+// endregion
+
+//region Main
+
+int main() {
+    map<string, PointerFunction> options;
+    inserirAcaoMenu(options);
+
+    string selecthd, input;
+    /*
+    while (cin >> selecthd) {
+        if (selecthd == "createhd") {
+
+        } else {
+            if (encontrou) {
+                while (cin >> input) {
+
+
+                    if (input == "createhd") {
+
+                    } else {
+                        options.find(input)->second();
+                    }
+                }
+            } else {
+                printf("Comando não é válido");
+            }
+
+        }
+        //openFile();
+        system("pause");
+
+        return 0;
+    }
+     */
+}
+// endregion
