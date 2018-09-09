4e5bae5a19aad3b083381ccefacbd71f49ee7a68
lhps <31356211+lhps@users.noreply.github.com>
2018-09-09 15:30:21 -0300

    att
    att

---
+++ cmake-build-debug/CMakeFiles/SO_Final.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "H:/FEI/6 Ciclo/SO/SO_Final/Content.cpp" "H:/FEI/6 Ciclo/SO/SO_Final/cmake-build-debug/CMakeFiles/SO_Final.dir/Content.cpp.obj"
+  "H:/FEI/6 Ciclo/SO/SO_Final/File.cpp" "H:/FEI/6 Ciclo/SO/SO_Final/cmake-build-debug/CMakeFiles/SO_Final.dir/File.cpp.obj"
+  "H:/FEI/6 Ciclo/SO/SO_Final/Folder.cpp" "H:/FEI/6 Ciclo/SO/SO_Final/cmake-build-debug/CMakeFiles/SO_Final.dir/Folder.cpp.obj"
+  "H:/FEI/6 Ciclo/SO/SO_Final/Manager.cpp" "H:/FEI/6 Ciclo/SO/SO_Final/cmake-build-debug/CMakeFiles/SO_Final.dir/Manager.cpp.obj"
+  "H:/FEI/6 Ciclo/SO/SO_Final/VirtualHD.cpp" "H:/FEI/6 Ciclo/SO/SO_Final/cmake-build-debug/CMakeFiles/SO_Final.dir/VirtualHD.cpp.obj"
+  "H:/FEI/6 Ciclo/SO/SO_Final/main.cpp" "H:/FEI/6 Ciclo/SO/SO_Final/cmake-build-debug/CMakeFiles/SO_Final.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
